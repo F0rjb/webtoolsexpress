4fd755340a9275b9526a8dbc176936eec0cd8d47
Ahmed Nasri <Ahmednasrigeo@gmail.com>
2023-06-27 12:40:28 +0100

    registration and login mecansime

---
+++ config/password.js
@@ -0,0 +1,27 @@
+const passport = require("passport");
+const passportJwt = require("passport-jwt");
+const User = require("../models/Users");
+
+const JwtStrategy = passportJwt.Strategy;
+const ExtractJwt = passportJwt.ExtractJwt;
+
+const options = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: "your-secret-key",
+};
+
+passport.use(
+  new JwtStrategy(options, async (jwtPayload, done) => {
+    try {
+      const user = await User.findById(jwtPayload.id);
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    } catch (error) {
+      console.error("Error in passport strategy:", error);
+      return done(error, false);
+    }
+  })
+);
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
+var morgan = require("morgan");
+
 // Connect to MongoDB using Mongoose
 mongoose
   .connect("mongodb://localhost/webtools", {
@@ -11,9 +13,18 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
+app.use(express.json());
 // Set up your Express middleware and routes here
 
 const port = process.env.PORT || 5000;
+
+const authRoutes = require("./routes/auth");
+
+// Use the auth routes
+app.use("/api/auth", morgan, authRoutes);
+
+// Add more routes as needed
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });

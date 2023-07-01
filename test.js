838ea47460baa81a8e8cdb4de114ec39fa04bf33
Ahmed Nasri <Ahmednasrigeo@gmail.com>
2023-07-01 19:10:27 +0100

    test errors

--- test.js
+++ test.js
@@ -1,111 +1,175 @@
 const request = require("supertest");
 const app = require("./app");
+const jwt = require("jsonwebtoken");
+const Users = require("./models/Users");
 
-require("./testSetup");
+describe("Authentication and Authorization", () => {
+  let userToken;
+  let adminToken;
 
-describe("User Registration", () => {
-  it("should register a new user", async () => {
-    const newUser = {
-      username: "testuser",
+  beforeAll(async () => {
+    // Create a user for testing
+    const user = new Users({
+      email: "testuser@example.com",
       password: "testpassword",
-    };
+      role: "user",
+    });
+    await user.save();
 
-    const response = await request(app)
-      .post("/api/register")
-      .send(newUser)
-      .expect(201);
+    // Generate a JWT token for the user
+    userToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
 
-    expect(response.body).toHaveProperty("token");
-  });
-
-  it("should return an error if user already exists", async () => {
-    const existingUser = {
-      email: "existing@example.com",
-      password: "existingpassword",
-    };
-
-    const response = await request(app)
-      .post("/api/register")
-      .send(existingUser)
-      .expect(400);
-
-    expect(response.body).toHaveProperty("message", "User already exists");
-  });
-});
-
-describe("User Login", () => {
-  it("should log in an existing user", async () => {
-    const userCredentials = {
-      email: "test@example.com",
-      password: "testpassword",
-    };
-
-    const response = await request(app)
-      .post("/api/login")
-      .send(userCredentials)
-      .expect(200);
-
-    expect(response.body).toHaveProperty("token");
-  });
-
-  it("should return an error if user not found", async () => {
-    const nonExistingUser = {
-      email: "nonexisting@example.com",
-      password: "nonexistingpassword",
-    };
-
-    const response = await request(app)
-      .post("/api/login")
-      .send(nonExistingUser)
-      .expect(404);
-
-    expect(response.body).toHaveProperty("message", "User not found");
-  });
-
-  it("should return an error if invalid credentials", async () => {
-    const invalidCredentials = {
-      email: "test@example.com",
-      password: "wrongpassword",
-    };
-
-    const response = await request(app)
-      .post("/api/login")
-      .send(invalidCredentials)
-      .expect(401);
-
-    expect(response.body).toHaveProperty("message", "Invalid credentials");
-  });
-});
-
-describe("Admin Registration", () => {
-  it("should register a new admin", async () => {
-    const newAdmin = {
-      username: "adminuser",
+    // Create an admin for testing
+    const admin = new Users({
+      email: "admin@example.com",
       password: "adminpassword",
-    };
+      role: "admin",
+    });
+    await admin.save();
 
-    const response = await request(app)
-      .post("/api/admin/register")
-      .set("Authorization", `Bearer your-admin-token`) // Replace with an actual admin token
-      .send(newAdmin)
-      .expect(201);
-
-    expect(response.body).toHaveProperty("token");
+    // Generate a JWT token for the admin
+    adminToken = jwt.sign({ id: admin._id }, process.env.JWT_SECRET);
   });
 
-  it("should return an error if admin already exists", async () => {
-    const existingAdmin = {
-      email: "existingadmin@example.com",
-      password: "existingadminpassword",
-      role: "admin",
-    };
+  afterAll(async () => {
+    // Clean up the database after tests
+    await Users.deleteMany();
+  });
 
-    const response = await request(app)
-      .post("/api/admin/register")
-      .set("Authorization", `Bearer your-admin-token`) // Replace with an actual admin token
-      .send(existingAdmin)
-      .expect(400);
+  describe("User Registration", () => {
+    it("should register a new user", async () => {
+      const newUser = {
+        email: "newuser@example.com",
+        password: "newpassword",
+        role: "user",
+      };
 
-    expect(response.body).toHaveProperty("message", "User already exists");
+      const response = await request(app)
+        .post("/api/auth/register")
+        .send(newUser)
+        .expect(201);
+
+      expect(response.body).toHaveProperty("token");
+      expect(response.body).toHaveProperty(
+        "message",
+        `${newUser.role} registered successfully`
+      );
+    });
+
+    it("should return an error if user already exists", async () => {
+      const existingUser = {
+        email: "testuser@example.com",
+        password: "existingpassword",
+        role: "user",
+      };
+
+      const response = await request(app)
+        .post("/api/auth/register")
+        .send(existingUser)
+        .expect(400);
+
+      expect(response.body).toHaveProperty("message", "User already exists");
+    });
+  });
+
+  describe("User Login", () => {
+    it("should log in an existing user", async () => {
+      const userCredentials = {
+        email: "testuser@example.com",
+        password: "testpassword",
+      };
+
+      const response = await request(app)
+        .post("/api/auth/login")
+        .send(userCredentials)
+        .expect(200);
+
+      expect(response.body).toHaveProperty("token");
+    });
+
+    it("should return an error if user not found", async () => {
+      const nonExistingUser = {
+        email: "nonexisting@example.com",
+        password: "nonexistingpassword",
+      };
+
+      const response = await request(app)
+        .post("/api/auth/login")
+        .send(nonExistingUser)
+        .expect(404);
+
+      expect(response.body).toHaveProperty("message", "User not found");
+    });
+
+    it("should return an error if invalid credentials", async () => {
+      const invalidCredentials = {
+        email: "testuser@example.com",
+        password: "wrongpassword",
+      };
+
+      const response = await request(app)
+        .post("/api/auth/login")
+        .send(invalidCredentials)
+        .expect(401);
+
+      expect(response.body).toHaveProperty("message", "Invalid credentials");
+    });
+  });
+
+  describe("Admin Registration", () => {
+    it("should register a new admin", async () => {
+      const newAdmin = {
+        email: "newadmin@example.com",
+        password: "newpassword",
+        role: "admin",
+      };
+
+      const response = await request(app)
+        .post("/api/auth/admin/register")
+        .set("Authorization", `Bearer ${adminToken}`)
+        .send(newAdmin)
+        .expect(201);
+
+      expect(response.body).toHaveProperty("token");
+      expect(response.body).toHaveProperty(
+        "message",
+        `${newAdmin.role} registered successfully`
+      );
+    });
+
+    it("should return an error if user already exists", async () => {
+      const existingAdmin = {
+        email: "admin@example.com",
+        password: "existingpassword",
+        role: "admin",
+      };
+
+      const response = await request(app)
+        .post("/api/auth/admin/register")
+        .set("Authorization", `Bearer ${adminToken}`)
+        .send(existingAdmin)
+        .expect(400);
+
+      expect(response.body).toHaveProperty("message", "User already exists");
+    });
+
+    it("should return an error if unauthorized", async () => {
+      const newAdmin = {
+        email: "unauthorizedadmin@example.com",
+        password: "newpassword",
+        role: "admin",
+      };
+
+      const response = await request(app)
+        .post("/api/auth/admin/register")
+        .set("Authorization", `Bearer ${userToken}`)
+        .send(newAdmin)
+        .expect(401);
+
+      expect(response.body).toHaveProperty(
+        "message",
+        "Unauthorized: Admin access required"
+      );
+    });
   });
 });
